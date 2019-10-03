ed1b47c239ebb8400e0e56aed215ada7e7be7f36
Frogman7 <Thermonuclear@sbcglobal.net>
2019-10-02 21:18:27 -0700

    Adding missing Jpeg classes

---
+++ Source/ImageSearep/Models/JpgImageData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSearep.Models
+{
+    public class JpgImageData : ImageDataBase
+    {
+        private const string PNGFileExtension = "jpg";
+
+        public JpgImageData(byte[] imageData)
+            : base(imageData)
+        {
+            this.Image = this.BytesToImage(imageData);
+        }
+
+        public override Image Image { get; protected set; }
+
+        public override bool ReplaceImage(byte[] newImageData)
+        {
+            if (newImageData.Length > this.ImageBinary.Length)
+            {
+                return false;
+            }
+
+            Array.Copy(newImageData, this.ImageBinary, newImageData.Length);
+
+            this.Image = this.BytesToImage(this.ImageBinary);
+
+            return true;
+        }
+
+        public override string FileExtension
+        {
+            get
+            {
+                return PNGFileExtension;
+            }
+        }
+
+        private Image BytesToImage(byte[] imageBytes)
+        {
+            Image image = null;
+
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                image = Image.FromStream(ms);
+            }
+
+            return image;
+        }
+    }
+}
---
+++ Source/ImageSearep/Models/JpgImageFinder.cs
@@ -0,0 +1,47 @@
+﻿using ImageSearep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSearep
+{
+    public class JpgImageFinder : IImageFinder
+    {
+        private readonly byte[] ImageHeaderStart;
+
+        private readonly byte[] ImageEOF;
+
+        public JpgImageFinder()
+        {
+            this.ImageHeaderStart = new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 };
+
+            this.ImageEOF = new byte[] { 0xFF, 0xD9 };
+        }
+
+        public Task<IEnumerable<EmbeddedImageInfo>> FindImages(byte[] binaryData)
+        {
+            return Task.Run(
+                () => this.FindJpgImages(binaryData));
+        }
+
+        private IEnumerable<EmbeddedImageInfo> FindJpgImages(byte[] binaryData)
+        {
+            IImageData imageData = null;
+
+            IList<EmbeddedImageInfo> embeddedImageInfos = new List<EmbeddedImageInfo>();
+
+            var matches = BinarySearch.Match(binaryData, this.ImageHeaderStart, this.ImageEOF);
+
+            foreach (var match in matches)
+            {
+                imageData = new JpgImageData(match.BinaryData);
+
+                embeddedImageInfos.Add(new EmbeddedImageInfo(imageData, match.StartIndex));
+            }
+
+            return embeddedImageInfos;
+        }
+    }
+}
